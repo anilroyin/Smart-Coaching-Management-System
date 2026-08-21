a47d0b7cdf9233cbfb96b530a84f3453d4e17b9e
Anil Roy <anilroy.cce@gmail.com>
2026-08-21 21:50:53 +0530

    Updated Auth Controllers

--- server/controllers/authController.js
+++ server/controllers/authController.js
@@ -46,7 +46,8 @@ export const login = async (req, res) => {
                 id: user._id,
                 name: user.name,
                 email: user.email,
-                role: user.role
+                role: user.role,
+                permissions: user.permissions
             }
         });
     } catch (error) {
