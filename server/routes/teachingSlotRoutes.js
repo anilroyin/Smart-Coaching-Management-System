05198c902f9344110b8b7e9233f73f3f207a35d8
Anil Roy <anilroy.cce@gmail.com>
2026-08-24 02:39:31 +0530

    update teaching slot route

--- server/routes/teachingSlotRoutes.js
+++ server/routes/teachingSlotRoutes.js
@@ -1,15 +1,47 @@
 import express from "express";
+
 import {
     createTeachingSlot,
     getTeachingSlots,
-    createMultipleTeachingSlots
+    createMultipleTeachingSlots,
+    getTeachingSlotsByTeacher
 } from "../controllers/teachingSlotController.js";
+
+import authMiddleware from "../middleware/authMiddleware.js";
+import permissionMiddleware from "../middleware/permissionMiddleware.js";
 
 const router = express.Router();
 
 // Create a teaching slot
-router.post("/", createTeachingSlot);
-router.post("/bulk", createMultipleTeachingSlots);
-router.get("/", getTeachingSlots);
+router.post(
+    "/",
+    authMiddleware,
+    permissionMiddleware("teachingSlots"),
+    createTeachingSlot
+);
+
+// Create multiple teaching slots
+router.post(
+    "/bulk",
+    authMiddleware,
+    permissionMiddleware("teachingSlots"),
+    createMultipleTeachingSlots
+);
+
+// Get all teaching slots
+router.get(
+    "/",
+    authMiddleware,
+    permissionMiddleware("teachingSlots"),
+    getTeachingSlots
+);
+
+// Get teaching slots of a particular teacher
+router.get(
+    "/teacher/:teacherId",
+    authMiddleware,
+    permissionMiddleware("teachingSlots"),
+    getTeachingSlotsByTeacher
+);
 
 export default router;
