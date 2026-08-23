72710b56360c0b8ce0b5bbc3885b430587fa2cff
Anil Roy <anilroy.cce@gmail.com>
2026-08-24 02:36:32 +0530

    updated course teacher route

--- server/routes/courseTeacherRoutes.js
+++ server/routes/courseTeacherRoutes.js
@@ -1,14 +1,28 @@
 import express from "express";
-import { assignTeacherToCourse } from "../controllers/courseTeacherController.js";
+
+import {
+    assignTeacherToCourse,
+    getCoursesByTeacher
+} from "../controllers/courseTeacherController.js";
+
 import authMiddleware from "../middleware/authMiddleware.js";
-import roleMiddleware from "../middleware/roleMiddleware.js";
+import permissionMiddleware from "../middleware/permissionMiddleware.js";
 
 const router = express.Router();
+
+// Get courses assigned to a particular teacher
+
+router.get(
+    "/teacher/:teacherId",
+    authMiddleware,
+    permissionMiddleware("teachers"),
+    getCoursesByTeacher
+);
 
 router.post(
     "/",
     authMiddleware,
-    roleMiddleware("admin"),
+    permissionMiddleware("teachers"),
     assignTeacherToCourse
 );
 
